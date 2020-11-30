3ece4628c6b395d4553b0c92e8416c27a93ff9e2
Kermo880 <kermo.henn@vikk.ee>
2020-11-30 15:19:36 +0200

    API search

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 // Import stylesheets
-import './style.css';
+import "./style.css";
 
 // Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+const appDiv = document.getElementById("app"); // Select element with id "app"
+
+function reqListener() {
+  appDiv.innerHTML = JSON.parse(this.responseText).extract_html; // Inject something into that element
+  console.log(this.responseText);
+}
+
+var oReq = new XMLHttpRequest();
+oReq.addEventListener("load", reqListener);
+oReq.open(
+  "GET",
+  "https://en.wikipedia.org/api/rest_v1/page/summary/Viljandi_County"
+);
+oReq.send();
